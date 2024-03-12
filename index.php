9ad25c7d5cad8e554ee90e2c82481dccc433a91a
Diego Manrique <manriqueespinosadiegofernando@gmail.com>
2024-03-12 08:46:18 -0500

    Responsive

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E&M Registrer</title>
-    <link rel="stylesheet" href="./styles.css">
+    <link rel="stylesheet" href="styles/styles.css">
     <link rel="icon" href="./Storage_pc/eym.png">
 </head>
 <body>
